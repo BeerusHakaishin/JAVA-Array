8da9d80980615dee3950a33d7359797a87099fc1
Ismaël Meziani <meziani-i@outlook.fr>
2021-11-30 10:59:36 +0100

    i achieve this f**** bi dimentionnal strings array

--- Movie.java
+++ Movie.java
@@ -1,19 +1,30 @@
 import java.util.Arrays;
 
-public class Movie {
-public static void main(String[] args) {
-		
-	String[] filmTitles = {"Le Royaume du Crâne de Cristal", "La Dernière Croisade","Le Temple maudit"};
-	String[] principalActors = {"Harrison Ford & Cate Blanchett","Harrison Ford & Sean Connery","Harrison Ford & Kate Capshaw"};
-    
-	String[][] movies;
+class Movie {
 	
-	movies = new String[][] {filmTitles, principalActors};
-	
-	for (int i = 0; i < filmTitles.length; i++) {
-		System.out.println("The film Title is : " + filmTitles[i] + " and the principal actors are : " + principalActors[i]);
-        }
+    public static void main(String[] args) {
+    	
+    	String[] filmTitles ={
+    		"Indiana Jones and the Kingdom of the Crystal Skull",
+    		"Indiana Jones and the Last Crusade",
+    		"Indiana Jones and the Temple of Doom"
+    	};
+    	
+    	String[][] actors ={
+    		{"Harrison Ford", "Cate Blanchett", "Meziani Ismaël"},
+    		{"Harrison Ford", "Sean Connery", "Meziani Ismaël"},
+    		{"Harrison Ford", "Kate Capshaw", "Meziani Ismaël Again !"},
+    	};
+    	
+
+      for (int i = 0; i < filmTitles.length; i++){
+    	  	System.out.println("In the movie "+ filmTitles[i] + ", the principals actors are : ");
+        
+      for (int j = 0; j < actors[i].length; j++){
+            System.out.println("-" + actors[i][j]);}  
+      };
       
+
+    }
 }
-}
-	
+
